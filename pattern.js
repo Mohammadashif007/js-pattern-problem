414d16fc6a72fdaddc9a3588d278fc00cb3b1e1e
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-02 17:59:33 +0600

    right triangle pattern done

--- pattern.js
+++ pattern.js
@@ -15,22 +15,40 @@
 
 // 2. Hollow squire pattern
 
+// let n = 5;
+// let string = "";
+// for(let i = 0; i < n; i++){
+//     for(let j = 0; j < n; j++){
+//         if(i == 0 || i == n-1){
+//             string += "*";
+//         }
+//         else {
+//             if(j == 0 || j == n-1){
+//                 string += "*";
+//             }
+//             else {
+//                 string += " ";
+//             }
+//         }
+//     }
+//     string += "\n";
+// }
+// console.log(string)
+
+
+// 3. Right triangle pattern 
+
 let n = 5;
 let string = "";
 for(let i = 0; i < n; i++){
-    for(let j = 0; j < n; j++){
-        if(i == 0 || i == n-1){
-            string += "*";
-        }
-        else {
-            if(j == 0 || j == n-1){
-                string += "*";
-            }
-            else {
-                string += " ";
-            }
-        }
+    for(let j = 0; j <= n- i; j++){
+        string += " ";
+    }
+    for(let k = 0; k <=i; k++){
+        string += "*";
     }
     string += "\n";
 }
 console.log(string)
+
+
